09eaa064d333811ea6b534fe2a5df22c7f2d0e20
siradaaaaaa <89377840+siradaaaaaa@users.noreply.github.com>
2022-01-07 14:25:53 +0700

    Create page.php

---
+++ page.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <style>
+      body {
+        background-color: green;
+      }
+      #box1{
+        width: 500px;
+        height: 500px;
+        background-color: red;
+      }
+      .box2{
+        width: 500px;
+        height: 500px;
+        background-color: blue;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="box1">
+    </div>
+    <div class="box2">
+    </div>
+  </body>
+</html>
